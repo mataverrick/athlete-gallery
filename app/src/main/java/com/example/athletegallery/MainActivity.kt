3cc710ef4b2a0efa9ec5b925c65f0dda864df956
mataverrick <mataveraerick@gmail.com>
2025-08-21 17:34:53 -0600

    low fidelity

--- app/src/main/java/com/example/athletegallery/MainActivity.kt
+++ app/src/main/java/com/example/athletegallery/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.athletegallery
 
 import android.os.Bundle
+import android.view.Display
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.athletegallery.ui.theme.AthleteGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +42,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AthleteGalleryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    DisplayLayout(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize().verticalScroll(rememberScrollState())
                     )
                 }
             }
@@ -31,17 +54,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DisplayImage() {
+    Column(modifier = Modifier.padding(bottom = 40.dp)) {
+        Image(
+            painter = painterResource(R.drawable.messi),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.width(230.dp).height(290.dp)
+        )
+    }
+}
+
+@Composable
+fun DisplayInformation(modifier: Modifier) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text = "Leonel Messi",
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "38 years old",
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun DisplayLayout(modifier: Modifier) {
+    Column(
+        modifier, horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        DisplayImage()
+        DisplayInformation(modifier = Modifier.fillMaxWidth())
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {},
+                contentPadding = PaddingValues(horizontal = 40.dp)
+            ) { Text(text = "Previous") }
+            Button(
+                onClick = {},
+                contentPadding = PaddingValues(horizontal = 50.dp)
+            ) { Text(text = "Next") }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AthleteGalleryTheme {
-        Greeting("Android")
+        DisplayLayout(modifier = Modifier.fillMaxSize())
     }
 }
